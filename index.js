a00f2b8c8ef3402d837ba59c33efefd0174cd721
Alec Montalbano <alec.montalbano@gmail.com>
2017-07-20 18:52:51 -0400

    added the morgan logger middleware

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+/*jshint esversion:6*/
 const express = require('express');
 const server = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
+//middleware imports
+const morgan = require('morgan');
+
+//wire up middleware
+server.use(morgan('dev'));
 
 server.get('/', (req, res) => {
      res.send('it works');
