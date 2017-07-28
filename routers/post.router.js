d706a8af8b55c3abc8d08afb3a832830996b6755
Alec Montalbano <alec.montalbano@gmail.com>
2017-07-27 20:01:15 -0400

    created post model but didnt write in it, and started to write post router but am unable to finish due to unfinished post model

--- routers/post.router.js
+++ routers/post.router.js
@@ -1,22 +1,43 @@
 /* jshint esversion:6*/
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/post.model');
 
 router.get('/posts', (req, res) => {
-  res.send('getting dem post!');
+  Post.find({}, function(err, posts){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      post: posts
+    });
+  });
 });
 router.get('/posts/:postId', (req, res) => {
-  res.send('get that special post!');
+  Post.find({_id : req.params.postId }, function(err, posts){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      posts: posts
+    });
+  });
 });
 
 router.post('/posts', (req, res) => {
   res.send('creating that new post requested!');
 });
 router.put('/posts/:postId', (req, res) => {
-  res.send('updates errweerr!');
+  Post.findOneAndUpdate({_id: req.params.postId}, req.body, function(err){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      msg: "Successfully updated post"
+    });
+  });
 });
 router.delete('/posts/:postId', (req, res) => {
-  res.send('delete dems!');
+  Post.findOneAndRemove({_id: req.params.postId}, function(err, removedPost){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      msg: "Post successfully removed"
+    });
+  });
 });
 
 module.exports = router;
